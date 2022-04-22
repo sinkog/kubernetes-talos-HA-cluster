c1db25fa450426295c46a1a7a5eb427fb6e5fdb3
Gábor Sinkó <gabor.sinko@mmsone.com>
2022-04-22 11:24:43 +0200

    include BaseOperationSystemInstallScripts

--- make.sh
+++ make.sh
@@ -1,2 +1,19 @@
 #!bin/bash
-bash nyers/create_vm.sh
+### incude dependencies
+if [ -d "BaseOperationSystemInstallScripts" ]; then
+  echo "git pull on the BaseOperationSystemInstallScripts directory"
+  if !(cd BaseOperationSystemInstallScripts && git pull && cd .. ;); then
+    echo "git pull failed" && exit 1
+  else 
+    echo "pulled"
+  fi
+else
+  echo "git clone BaseOperationSystemInstallScripts"
+  if !(git clone https://github.com/sinkog/BaseOperationSystemInstallScripts.git); then
+    echo "git clone failed"
+  fi
+fi
+
+
+
+#bash nyers/create_vm.sh
