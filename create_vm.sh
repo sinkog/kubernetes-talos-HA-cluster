64c63a618fd49ece9d4e04f1dcc6d0fbbb25d546
Gábor Sinkó <gabor.sinko@mmsone.com>
2022-05-18 13:54:22 +0200

    3 servers

--- create_vm.sh
+++ create_vm.sh
@@ -1,27 +1,31 @@
-PROXMOX_ACCESS="root@10.10.99.26"
+PROXMOX_USER="root"
+PROXMOX_SERVERS="10.10.99.21,10.10.99.22,10.10.99.23"
+ IFS=',' read -ra PROXMOX_MYARRAY <<< $PROXMOX_SERVERS
 TALOS_NAME='TalosDevops'
 ROOT_STORAGE="scsi0 ssd-vm:64"
-CEPH_STORAGE="scsi1 local-zfs:128"
+CEPH_STORAGE="scsi1 local-zfs:2048"
 TALOS_ISO="iso:iso/talos-amd64.iso"
 TALOS=\
-"771,${TALOS_NAME}Master01,08:00:27:A4:A4:A1,8192,2048,1,c
- 772,${TALOS_NAME}Master02,08:00:27:A4:A4:A2,8192,2048,1,c
- 773,${TALOS_NAME}Master03,08:00:27:A4:A4:A3,8192,2048,1,c
- 781,${TALOS_NAME}Worker01,08:00:27:A4:A4:B1,12288,2048,2,w
- 782,${TALOS_NAME}Worker02,08:00:27:A4:A4:B2,12288,2048,2,w
- 783,${TALOS_NAME}Worker03,08:00:27:A4:A4:B3,12288,2048,2,w"
+"771,${TALOS_NAME}Master01,08:00:27:A4:A4:A1,8192,2048,1,c,${PROXMOX_MYARRAY[0]}
+ 772,${TALOS_NAME}Master02,08:00:27:A4:A4:A2,8192,2048,1,c,${PROXMOX_MYARRAY[1]}
+ 773,${TALOS_NAME}Master03,08:00:27:A4:A4:A3,8192,2048,1,c,${PROXMOX_MYARRAY[2]}
+ 781,${TALOS_NAME}Worker01,08:00:27:A4:A4:B1,12288,2048,2,w,${PROXMOX_MYARRAY[0]}
+ 782,${TALOS_NAME}Worker02,08:00:27:A4:A4:B2,12288,2048,2,w,${PROXMOX_MYARRAY[1]}
+ 783,${TALOS_NAME}Worker03,08:00:27:A4:A4:B3,12288,2048,2,w,${PROXMOX_MYARRAY[2]}"
 
- SSH_COMMAND="echo 'start'"
+for i in "${PROXMOX_MYARRAY[@]}"; do
+   echo -n "echo 'start'" > ${WORKDIR}/SSH_COMMAND_${i}.sh
+done
 
 for word in $TALOS; do
  IFS=',' read -ra MYARRAY <<< $word
- SSH_COMMAND="${SSH_COMMAND}; qm stop ${MYARRAY[0]}; qm destroy ${MYARRAY[0]}"
+ echo -n "; qm stop ${MYARRAY[0]}; qm destroy ${MYARRAY[0]}" >>  ${WORKDIR}/SSH_COMMAND_${MYARRAY[7]}.sh
 done
  
 for word in $TALOS; do
  IFS=',' read -ra MYARRAY <<< $word
  echo "Creating Talos virtual machine ${MYARRAY[0]}"
- SSH_COMMAND="${SSH_COMMAND}; qm create  ${MYARRAY[0]} \
+ echo -n "; qm create  ${MYARRAY[0]} \
   --name ${MYARRAY[1]} \
   --onboot 1 \
   --startup order=${MYARRAY[5]},up=180 \
@@ -36,13 +40,15 @@ for word in $TALOS; do
   --onboot yes \
   --start true \
   --sockets 4 \
-  --cores 4 "
+  --cores 4 " >> ${WORKDIR}/SSH_COMMAND_${MYARRAY[7]}.sh
  if [ "${MYARRAY[6]}" == "w" ]; then
-  SSH_COMMAND="${SSH_COMMAND} \
-  --${CEPH_STORAGE} "
+   echo -n " --${CEPH_STORAGE} " >> ${WORKDIR}/SSH_COMMAND_${MYARRAY[7]}.sh
  fi
 done
 
-ssh ${PROXMOX_ACCESS} ${SSH_COMMAND}
+for i in "${PROXMOX_MYARRAY[@]}"; do
+   ssh ${PROXMOX_USER}@${i} "`cat ${WORKDIR}/SSH_COMMAND_${i}.sh`"
+done
+
 
 echo "Finished..."
